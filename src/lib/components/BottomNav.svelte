dfae396d4b0aab33b8f780331f45bbab01d3d693
Danny Cisco <gumballelectronics@gmail.com>
2024-07-17 16:30:31 +1000

    nav buttons on main page

---
+++ src/lib/components/BottomNav.svelte
@@ -0,0 +1,23 @@
+<script>
+	// Define any needed logic here
+</script>
+
+<nav class="w-full flex justify-around items-center p-4 shadow-lg">
+	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">+</button>
+	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">IN</button>
+	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">DO</button>
+	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">DONE</button>
+	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">SEARCH</button>
+	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">BISHOP</button>
+	<button class="bg-yellow-400 text-white py-2 px-4 rounded-full">ME</button>
+</nav>
+
+<style>
+	nav {
+		z-index: 10;
+	}
+	button {
+		flex: 1;
+		margin-inline: 10px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
 	import '../app.postcss';
 
+	import BottomNav from '$lib/components/BottomNav.svelte';
+
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<div
-	class="container h-full mx-auto flex justify-center text-black items-center bg-white rounded-3xl"
->
-	<slot />
+<div class="relative h-full flex flex-col">
+	<div
+		class="flex-1 mt-4 container mx-auto flex justify-center text-black items-center bg-white rounded-3xl"
+	>
+		<slot />
+	</div>
+	<BottomNav />
 </div>
