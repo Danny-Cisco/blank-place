2b2643a2222c97f44f7ce0d1fe036574d6f90759
Danny Cisco <gumballelectronics@gmail.com>
2024-07-17 16:32:18 +1000

    nav buttons link to pages

--- src/lib/components/BottomNav.svelte
+++ src/lib/components/BottomNav.svelte
@@ -3,21 +3,25 @@
 </script>
 
 <nav class="w-full flex justify-around items-center p-4 shadow-lg">
-	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">+</button>
-	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">IN</button>
-	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">DO</button>
-	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">DONE</button>
-	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">SEARCH</button>
-	<button class="bg-gray-400 text-white py-2 px-4 rounded-full">BISHOP</button>
-	<button class="bg-yellow-400 text-white py-2 px-4 rounded-full">ME</button>
+	<a href="/plus" class="bg-gray-400 text-white py-2 px-4 rounded-full text-center">+</a>
+	<a href="/in" class="bg-gray-400 text-white py-2 px-4 rounded-full text-center">IN</a>
+	<a href="/do" class="bg-gray-400 text-white py-2 px-4 rounded-full text-center">DO</a>
+	<a href="/done" class="bg-gray-400 text-white py-2 px-4 rounded-full text-center">DONE</a>
+	<a href="/search" class="bg-gray-400 text-white py-2 px-4 rounded-full text-center">SEARCH</a>
+	<a href="/bishop" class="bg-gray-400 text-white py-2 px-4 rounded-full text-center">BISHOP</a>
+	<a href="/me" class="bg-yellow-400 text-white py-2 px-4 rounded-full text-center">ME</a>
 </nav>
 
 <style>
 	nav {
 		z-index: 10;
 	}
-	button {
+	a {
 		flex: 1;
 		margin-inline: 10px;
+		text-decoration: none;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
