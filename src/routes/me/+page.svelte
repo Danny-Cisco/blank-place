c496f77580a6b6c49580d62de0755ced4fd80314
Danny Cisco <gumballelectronics@gmail.com>
2024-07-17 17:50:16 +1000

    me page

--- src/routes/me/+page.svelte
+++ src/routes/me/+page.svelte
@@ -1,25 +1,25 @@
-<div class="bg-gray-100 flex justify-center items-center min-h-screen">
+<div class="flex justify-center items-center">
 	<div class="flex flex-col items-start p-8 bg-white rounded-lg w-full max-w-lg">
 		<h1 class="text-xxl font-bold mb-8">User Name</h1>
-		<div class="space-y-4 flex flex-col">
-			<a href="/my-emails" class="bg-gray-700 text-white py-2 px-4 rounded-full w-48 text-left"
+		<div class="space-y-4 flex flex-col justify-items-stretch">
+			<a href="/my-emails" class="bg-gray-700 text-white py-2 px-4 rounded-full w-full text-left"
 				>My Emails</a
 			>
-			<button class="bg-gray-700 text-white py-2 px-4 rounded-full w-48 text-left"
-				>Close My Account</button
-			>
-			<a href="/login" class="bg-gray-700 text-white py-2 px-4 rounded-full w-48 text-left"
+
+			<a href="/login" class="bg-gray-700 text-white py-2 px-4 rounded-full w-full text-left"
 				>Log Out</a
 			>
 		</div>
+		<div class="h-[2px] my-4 w-full bg-black"></div>
+		<p class="text-red-500 mb-4">Danger Zone</p>
+		<a href="/close-account" class="bg-red-700 text-white py-2 px-4 rounded-full w-48 text-left"
+			>Close My Account</a
+		>
 	</div>
 </div>
 
 <style>
 	button {
 		transition: background-color 0.3s;
-	}
-	button:hover {
-		background-color: #4b5563; /* Tailwind gray-600 */
 	}
 </style>
