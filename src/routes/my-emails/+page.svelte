4e9e773f1f1fb214f9306e75e8480b7d4512c975
Danny Cisco <gumballelectronics@gmail.com>
2024-07-17 17:28:49 +1000

    my-emails page works

---
+++ src/routes/my-emails/+page.svelte
@@ -0,0 +1,13 @@
+<h1 class="mb-8 font-bold mb-4">My Emails</h1>
+<p class="mb-8 max-w-lg">
+	You currently have no email connected, follow the relevant link below to get your emails coming to
+	The Place
+</p>
+<div class="flex justify-around mb-6 space-x-4">
+	<button type="button" class="bg-gray-700 flex-1 text-white py-2 px-4 rounded-full"
+		>Continue with Google</button
+	>
+	<button type="button" class="bg-gray-700 flex-1 text-white py-2 px-4 rounded-full"
+		>Continue with Microsoft</button
+	>
+</div>
