6990688b1307852d14a28e2c07969d5386cff1a0
Danny Cisco <gumballelectronics@gmail.com>
2024-07-17 17:57:43 +1000

    are you sure button

---
+++ src/routes/close-account/+page.svelte
@@ -0,0 +1,10 @@
+<div class="flex justify-center items-center">
+	<div class="flex flex-col items-start p-8 bg-white rounded-lg w-full max-w-lg">
+		<h1 class="text-xxl font-bold mb-8">Close Account?</h1>
+
+		<a
+			href="/close-account"
+			class="bg-red-700 text-white py-2 px-4 rounded-full mx-auto w-48 text-center">Are you Sure?</a
+		>
+	</div>
+</div>
