db1e6b769c73d404088a5d7252b88b2bc801a249
Danny Cisco <gumballelectronics@gmail.com>
2024-07-29 14:08:52 +1000

    Built the top bar

---
+++ src/lib/components/topBar/SearchBar.svelte
@@ -0,0 +1,6 @@
+<div
+	class="flex flex-col relative items-center justify-center rounded-full bg-white text-black overflow-hidden h-[50px]"
+>
+	<div class="absolute left-[15px] text-[24px]">🔎</div>
+	<input class="bg-white w-[102%] pl-[60px] h-[52px] my-[-2px]" placeholder="Search..." />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.postcss';
 
 	import BottomNav from '$lib/components/BottomNav.svelte';
+	import TopBar from '$lib/components/topBar/TopBar.svelte';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -10,8 +11,9 @@
 </script>
 
 <div class="relative h-full flex flex-col px-4">
+	<TopBar />
 	<div
-		class="flex-1 mt-4 mx-auto w-full flex flex-col justify-start text-black items-center bg-white rounded-3xl"
+		class="flex-1 mt-2 mx-auto w-full flex flex-col justify-start text-black items-center bg-white rounded-3xl"
 	>
 		<slot />
 	</div>
